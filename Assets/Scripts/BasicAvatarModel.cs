2ee4cbee63faaa81b73c86b1ff67f0fe5a42972b
lee <liou@gmx.at>
2017-12-01 14:49:04 +0100

    start

---
+++ Assets/Scripts/BasicAvatarModel.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Windows.Kinect;
+using System;
+
+public abstract class BasicAvatarModel : MonoBehaviour {
+
+
+    public abstract Quaternion applyRelativeRotationChange(JointType jt, Quaternion initialModelJointRotation, int player);
+
+    public abstract Quaternion getRawWorldRotation(JointType jt, int player);
+
+    public abstract Vector3 getRawWorldPosition(JointType jt, int player);
+}
---
+++ Assets/Scripts/UCharacterAvatarController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using Windows.Kinect;
+
+public class UCharacterAvatarController :  BasicAvatarController
+{
+
+	public override void Start(){
+
+        // find transforms of model
+        SpineBase = GameObject.Find("joint_HipMaster").transform;
+        SpineMid = GameObject.Find("joint_TorsoA").transform;
+        Neck = GameObject.Find("joint_Neck").transform;
+        Head = GameObject.Find("joint_Head").transform;
+        ShoulderLeft = GameObject.Find("joint_ShoulderLT").transform;
+        ElbowLeft = GameObject.Find("joint_ElbowLT").transform;
+        //WristLeft = GameObject.Find("joint_HandLT").transform;
+        HandLeft = GameObject.Find("joint_HandLT").transform;
+        ShoulderRight = GameObject.Find("joint_ShoulderRT").transform;
+        ElbowRight = GameObject.Find("joint_ElbowRT").transform;
+        //WristRight = GameObject.Find("joint_RightHand").transform;
+        HandRight = GameObject.Find("joint_HandRT").transform;
+        HipLeft = GameObject.Find("joint_HipLT").transform;
+        KneeLeft = GameObject.Find("joint_KneeLT").transform;
+        AnkleLeft = GameObject.Find("joint_FootLT").transform;
+        FootLeft = GameObject.Find("joint_ToeLT").transform;
+        HipRight = GameObject.Find("joint_HipRT").transform;
+        KneeRight = GameObject.Find("joint_KneeRT").transform;
+        AnkleRight = GameObject.Find("joint_FootRT").transform;
+        FootRight = GameObject.Find("joint_ToeRT").transform;
+        SpineShoulder = GameObject.Find("joint_TorsoB").transform;
+        //HandTipLeft = GameObject.Find("joint_LeftHandIndex1").transform;
+        //ThumbLeft = GameObject.Find("joint_LeftHandThumb1").transform;
+        //HandTipRight = GameObject.Find("joint_RightHandIndex1").transform;
+        //ThumbRight = GameObject.Find("joint_RightHandThumb1").transform;
+
+        base.Start();
+    }
+
+    public virtual void Update()
+    {
+        base.Update();
+    }
+}
