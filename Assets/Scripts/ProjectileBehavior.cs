16b6a20a2f1a8a69726353dc24cfd95601582627
lee <liou@gmx.at>
2018-01-09 14:03:53 +0100

    projectile

---
+++ Assets/Scripts/ProjectileBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileBehavior : MonoBehaviour {
+
+    public float projectileSpeed;
+    public float lifeTime;//maybe change to distance
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+        lifeTime -= Time.deltaTime;
+        if (lifeTime < 0)
+            Destroy(gameObject);
+        transform.Translate(Vector3.right * projectileSpeed * Time.deltaTime);
+    }
+}
