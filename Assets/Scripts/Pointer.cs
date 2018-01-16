1bab74cf369c0f779121b9d2433c51edcdc65425
GamesLab <s.kreisig@web.de>
2018-01-16 17:23:44 +0100

    added pointer ui support

--- Assets/Scripts/Pointer.cs
+++ Assets/Scripts/Pointer.cs
@@ -1,13 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class Pointer : MonoBehaviour {
-
 	// Use this for initialization
     private Camera _camera;
     private Renderer _rend;
     public Color normColor, activeColor;
+
 	void Start () {
         _camera = GameObject.Find("Main Camera").GetComponent<Camera>();
         _rend = GetComponent<Renderer>();
@@ -17,8 +19,9 @@ public class Pointer : MonoBehaviour {
 	void Update () {
         if (GestureHandler.Instance.detectPlayer())
         {
+            GestureHandler.Instance.calcPositions();
             Vector2 handPos = GestureHandler.Instance.getMappedRightHandPosition();
-            transform.position = _camera.ViewportToWorldPoint(new Vector3(handPos.x, handPos.y, 1));
+            transform.position = _camera.ViewportToWorldPoint(new Vector3(handPos.x, handPos.y, 89));
 
             if (GestureHandler.Instance.getRightHandState())
             {
@@ -27,11 +30,35 @@ public class Pointer : MonoBehaviour {
             else
             {
                 _rend.material.color = activeColor;
+
             }
+
         }
         else
         {
            // transform.position = new Vector3(0, -100, 0);
         }
 	}
+
+    void RaycastUIObjects()
+    {
+        var pointer = new PointerEventData(EventSystem.current);
+        // convert to screen space
+        pointer.position = Camera.main.WorldToScreenPoint(transform.position);
+        var raycastResults = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(pointer, raycastResults);
+        foreach (RaycastResult result in raycastResults)
+        {
+            Debug.Log("Hit2 " + raycastResults.Count + result.gameObject.name);
+
+            /*
+            Button b = result.gameObject.GetComponent<Button>();
+            if (b != null)
+            {
+                b.onClick.Invoke();
+            }
+             * */
+        }
+    }
+
 }
